c736c335bbb1991713ccef0febc8c7b906059274
GreenVenom77 <hosome2222@gmail.com>
2024-04-26 15:27:32 +0300

    removed os.remove 2

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -11,4 +11,3 @@ def test_AhoCorasickFilter(test_data, args):
     args = test_data['args']
     AhoCorasickFilter(args.bad_words_file)
     # Clean up the generated files after all tests are done
-    os.remove("Outputs")
