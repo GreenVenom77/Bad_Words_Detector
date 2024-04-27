10c9d0a8cb1af1a15f9860e0431530a3a00dd931
GreenVenom77 <hosome2222@gmail.com>
2024-04-27 16:11:01 +0300

    Changed the tests to fit the new structuring

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -7,6 +7,9 @@ from filter import AhoCorasickFilter, RegexFilter
 
 def test_filters(test_data, badwords_list):
     bad_words = test_data['badwords_list']
-    AhoCorasickFilter(bad_words)
-    RegexFilter(bad_words)
-    # Clean up the generated files after all tests are done
+
+    aho_filter = AhoCorasickFilter()
+    regex_filter = RegexFilter()
+
+    aho_filter.prepare(bad_words)
+    regex_filter.prepare(bad_words)
